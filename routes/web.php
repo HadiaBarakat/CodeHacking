59f79bb2f62f65593664525f00c8af19050e5bd7
Anyone-dot <hadiabarakat7@gmail.com>
2020-04-04 18:37:31 +0300

    admin master pase setup:

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,8 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('admin/users', 'AdminUsersController');
+Route::resource('admin/posts', 'AdminPostsController');
 
+Route::get('/admin', function (){
+    return view('admin.index');
+});

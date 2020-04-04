969188c7f9c00925abeed3f2170af3b6cd97223a
Anyone-dot <hadiabarakat7@gmail.com>
2020-04-04 17:25:31 +0300

    web mix

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('admin/users', 'AdminUsersController');
+
